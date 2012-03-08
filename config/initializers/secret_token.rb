68f53705646627c4b6d5f1b071a6cb46e49626ff
Eric Mitjans <erikmitjans@gmail.com>
2012-03-08 19:41:55 +0100

    rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoCMS::Application.config.secret_token = '213427a3b05d28cbc119962d125066165e93051677f8552f759073068d7a4ca67118e5b28ece3a0ed03898f3b1d2fe2c6bf35a32ed105a1521fb636b06fe0a13'
